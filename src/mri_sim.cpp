a241362df5831745b0dd7b580f9a1f2442688589
efe tiryaki <m.efetiryaki@gmail.com>
2018-12-29 15:32:28 +0300

    plotgui immigrated

--- src/mri_sim.cpp
+++ src/mri_sim.cpp
@@ -7,13 +7,14 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 #include <Eigen/Core>
-#include "OdeSolver.h"
-#include "RungeKutta.h"
-#include "SimplePendulumSolver.h"
+#include "ode_solver/OdeSolver.h"
+#include "ode_solver/RungeKutta.h"
+#include "ode_solver/SimplePendulumSolver.h"
+
 
 #include "PlotGui.h"
-#include <QApplication>
 
 using namespace std;
 using namespace ODE;
@@ -36,11 +37,20 @@ int main()
 
   int argc;
   char **argv;
-
   QApplication app(argc, argv);
   PlotGui gui;
 
   gui.plot(t,solver.getSolution().col(0));
+  gui.fillBelow(QColor(0,0,255,20));
+  gui.holdOn();
+  gui.scatter(t,solver.getSolution().col(1),Qt::red);
+  std::vector<std::string> legendNames(2);
+  legendNames[0] = "v";
+  legendNames[1] = "a" ;
+  gui.legend(legendNames);
+  gui.setAxis(0.0,10.0,-5.0,5.0);
   gui.show();
+
+
   return app.exec();
 }
